ee408b7b971fe964bec08b492686119edde52b81
[dyad] <judestringz@gmail.com>
2025-09-25 02:25:28 +0100

    [dyad] Replaced abstract pink circles with actual `Flower` icons from `lucide-react` in the `MovingFlowers` component - wrote 1 file(s)

--- src/components/MovingFlowers.tsx
+++ src/components/MovingFlowers.tsx
@@ -1,20 +1,21 @@
 "use client";
 
 import React from 'react';
+import { Flower } from 'lucide-react'; // Importing the Flower icon
 
 const MovingFlowers: React.FC = () => {
-  const flowerClasses = "absolute w-16 h-16 sm:w-20 sm:h-20 md:w-24 md:h-24 rounded-full bg-rose-200 opacity-70 blur-sm animate-float z-0";
+  const flowerClasses = "absolute text-rose-400 opacity-80 animate-float z-0"; // Adjusted classes for icon
 
   return (
     <>
       {/* Top-Left Flower */}
-      <div className={`${flowerClasses} top-4 left-4 animate-delay-100`}></div>
+      <Flower size={48} className={`${flowerClasses} top-4 left-4 animate-delay-100`} />
       {/* Top-Right Flower */}
-      <div className={`${flowerClasses} top-4 right-4 animate-delay-200`}></div>
+      <Flower size={48} className={`${flowerClasses} top-4 right-4 animate-delay-200`} />
       {/* Bottom-Left Flower */}
-      <div className={`${flowerClasses} bottom-4 left-4 animate-delay-300`}></div>
+      <Flower size={48} className={`${flowerClasses} bottom-4 left-4 animate-delay-300`} />
       {/* Bottom-Right Flower */}
-      <div className={`${flowerClasses} bottom-4 right-4 animate-delay-400`}></div>
+      <Flower size={48} className={`${flowerClasses} bottom-4 right-4 animate-delay-400`} />
     </>
   );
 };
